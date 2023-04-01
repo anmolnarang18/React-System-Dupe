f90dd7e1612e815eac726f86bc9671cb7c2d4342
Jitesh's Mac <macbookair@Macbooks-MacBook-Air.local>
2023-04-02 00:10:57 +0530

    final project

--- BE/shared/constants.js
+++ BE/shared/constants.js
@@ -2,7 +2,7 @@ module.exports = {
   SECRET_KEY: "superConfidentialToken",
 
   DB_URI:
-    "mongodb+srv://jiteshs:jiteshs7@cluster0.5h1dazv.mongodb.net/task?retryWrites=true&w=majority",
+    "mongodb+srv://jiteshs:*****@cluster0.5h1dazv.mongodb.net/task?retryWrites=true&w=majority",
 
   EMAIL_VALIDATION:
     /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/,
